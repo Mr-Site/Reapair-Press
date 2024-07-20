9ffa63376f42129bad44c5981ec8fa76a8375d0e
Mr-Site <matinayoobi00@gmail.com>
2024-07-20 23:58:34 +0330

    first commit

--- JS/main.js
+++ JS/main.js
@@ -66,3 +66,20 @@ function stopSlide() {
 function resumeSlide() {
   intrevalSlider = setInterval("plusSlides(1)", 5000);
 }
+
+// s7 menu item list
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
