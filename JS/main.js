7b898c090b0ba2c3bd5b8f793df99f29b57c06e0
Mr-Site <matinayoobi00@gmail.com>
2024-07-22 23:13:09 +0330

    first commit

--- JS/main.js
+++ JS/main.js
@@ -24,29 +24,6 @@ function removeServicesSubmenu() {
   document.getElementById("fa-caret-down").style.color = "#868484";
 }
 
-// change page
-
-function openPage(pageName, elmnt, kadr, borderBtm, paint) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.border = "";
-    tablinks[i].style.borderBottom = "";
-    tablinks[i].style.color = "#868684";
-  }
-  document.getElementById(pageName).style.display = "block";
-  elmnt.style.border = kadr;
-  elmnt.style.borderBottom = borderBtm;
-  elmnt.style.color = paint;
-}
-
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
-
 // home page
 
 // main slide show
@@ -108,3 +85,33 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+// s12 number counter
+
+const counters = document.querySelectorAll(".count");
+const speed = 200;
+
+// The code to start the animation is now wrapped in a function
+
+const startCounters = () => {
+  counters.forEach((counter) => {
+    const updateCount = () => {
+      const target = parseInt(+counter.getAttribute("data-target"));
+      const count = parseInt(+counter.innerText);
+      const increment = Math.trunc(target / speed);
+      if (count < target) {
+        counter.innerText = count + increment;
+        setTimeout(updateCount, 1);
+      } else {
+        count.innerText = target;
+      }
+    };
+    updateCount();
+  });
+};
+
+// On the first scroll in this window, call the function to start the counters
+
+window.addEventListener("scroll", startCounters, {
+  once: true,
+});
